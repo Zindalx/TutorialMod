28bf86e1751c7d872c353de2d9ce03aaea19b6ed
Zindalx <snoodnoodles@hotmail.com>
2023-04-26 11:53:14 -0400

    Added creative mode tab for custom items

--- src/main/java/net/zindal/tutorialmod/item/ModItems.java
+++ src/main/java/net/zindal/tutorialmod/item/ModItems.java
@@ -1,6 +1,5 @@
 package net.zindal.tutorialmod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,10 +12,10 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, Tutorialmod.MOD_ID);
 
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
