deb79bce91410a45822a4636331181d6d4bbfd31
Zindalx <snoodnoodles@hotmail.com>
2023-04-26 14:03:24 -0400

    Added block for Zircon and ore variants(Ore,Deepslate, Netherrack, Endstone)
    Added textures for new blocks mentioned above
    
    Added new blocks to creative mode tab

--- src/main/java/net/zindal/tutorialmod/Tutorialmod.java
+++ src/main/java/net/zindal/tutorialmod/Tutorialmod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.zindal.tutorialmod.block.ModBlocks;
 import net.zindal.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -30,6 +31,7 @@ public class Tutorialmod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
